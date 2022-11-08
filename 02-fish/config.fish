1b847a5aefe981031e5aca755e52f20903752f9a
Daniel Kalintsev <d.kalintsev@gmail.com>
2022-11-08 11:10:09 +0100

    fix(mcfly): disable mcfly

--- 02-fish/config.fish
+++ 02-fish/config.fish
@@ -3,5 +3,5 @@
 export ITERM_ENABLE_SHELL_INTEGRATION_WITH_TMUX=YES
 
 starship init fish | source
-mcfly init fish | source
+# mcfly init fish | source
 source "$(brew --prefix asdf)"/libexec/asdf.fish
