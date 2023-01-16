a293a2ccb460e3836a9859d1b06f793eec41d3a2
Daniel Kalintsev <d.kalintsev@gmail.com>
2023-01-16 21:02:35 +0100

    feat(nvim): add diffview plugin

---
+++ nvim/user/plugins/diffview.lua
@@ -0,0 +1,18 @@
+return {
+  default_args = {
+    DiffviewFileHistory = { "%" },
+  },
+  hooks = {
+    diff_buf_read = function()
+      vim.wo.wrap = false
+      vim.wo.list = false
+      vim.wo.colorcolumn = ""
+    end,
+  },
+  enhanced_diff_hl = true,
+  keymaps = {
+    view = { q = "<Cmd>DiffviewClose<CR>" },
+    file_panel = { q = "<Cmd>DiffviewClose<CR>" },
+    file_history_panel = { q = "<Cmd>DiffviewClose<CR>" },
+  },
+}
