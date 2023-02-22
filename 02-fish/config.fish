180d8c154bea35f963561988e22fbf501457cd2f
Daniel Kalintsev <d.kalintsev@gmail.com>
2023-02-22 10:42:27 +0100

    feat: use catppuccin_mocha in kitty, fish, starship

--- 02-fish/config.fish
+++ 02-fish/config.fish
@@ -3,10 +3,12 @@
 export ITERM_ENABLE_SHELL_INTEGRATION_WITH_TMUX=YES
 
 starship init fish | source
-# mcfly init fish | source
 
 function weather --description 'check the weather'
   curl wttr.in/~Berlin;
 end
 
 source "$(brew --prefix asdf)"/libexec/asdf.fish
+
+fish_config theme choose "Catppuccin Mocha"
+
--- fisher/install.fish
+++ fisher/install.fish
@@ -4,3 +4,4 @@ for plugin in (cat "$DOTFILES/fisher/plugins")
   echo "[fisher] Installing plugin $plugin..."
   fisher install "$plugin"
 end
+
