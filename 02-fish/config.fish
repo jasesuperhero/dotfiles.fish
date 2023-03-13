74e4e201286f25ee3fa86e624f19ca7dbd937931
Daniel Kalintsev <d.kalintsev@gmail.com>
2023-03-13 14:39:01 +0100

    fix: problem with brew after loading fish

--- 02-fish/config.fish
+++ 02-fish/config.fish
@@ -1,10 +1,6 @@
 #!/usr/bin/env fish
 
-if status is-interactive
+if command -qa brew and status is-interactive
   set ZELLIJ_AUTO_ATTACH true
   eval (zellij setup --generate-auto-start fish | string collect)
-end
-
-if set -q C_THEME
-  # update_theme $C_THEME
 end
