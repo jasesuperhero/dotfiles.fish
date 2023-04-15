d4cbc98390228997a8775c2b235c25c783465a47
Daniel Kalintsev <d.kalintsev@gmail.com>
2023-04-15 16:01:48 +0200

    feat(nvim): add tab buffer list mapping

--- nvim/user/mappings.lua
+++ nvim/user/mappings.lua
@@ -1,3 +1,5 @@
+local astro_utils = require "astronvim.utils"
+
 local mappings = {
   n = {
     -- disable default bindings
@@ -10,6 +12,20 @@ local mappings = {
     ["<Down>"] = { function() require("smart-splits").resize_down(2) end, desc = "Resize split down" },
     ["<Left>"] = { function() require("smart-splits").resize_left(2) end, desc = "Resize split left" },
     ["<Right>"] = { function() require("smart-splits").resize_right(2) end, desc = "Resize split right" },
+    -- Movement
+    ["<Tab>"] = {
+      function()
+        if #vim.t.bufs > 1 then
+          require("telescope.builtin").buffers {
+            sort_mru = true,
+            ignore_current_buffer = true,
+          }
+        else
+          astro_utils.notify "No other buffers open"
+        end
+      end,
+      desc = "Switch Buffers",
+    },
   },
 }
 
