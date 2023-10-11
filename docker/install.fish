f287d68523c190e2a97aeacab9d0ca45bdd14bc6
Daniel Kalintsev <d.kalintsev@gmail.com>
2023-10-11 23:42:27 +0200

    fix: formatting of the fish script installation for the docker

--- docker/install.fish
+++ docker/install.fish
@@ -1,3 +1,6 @@
 #!/usr/bin/env fish
 
-curl -sL https://raw.githubusercontent.com/docker/cli/master/contrib/completion/fish/docker.fish -o ~/.config/fish/completions/docker.fish
+curl \
+  -sL \
+  https://raw.githubusercontent.com/docker/cli/master/contrib/completion/fish/docker.fish \
+  -o ~/.config/fish/completions/docker.fish
