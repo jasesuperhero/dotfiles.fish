32ecf1eeefa9e6f3e169133c13b11fb2d0257238
Daniel Kalintsev <d.kalintsev@gmail.com>
2023-12-17 12:30:50 +0100

    feat(nvim): lsp and formatting for typescript

--- nvim/user/plugins/lsp/mason.lua
+++ nvim/user/plugins/lsp/mason.lua
@@ -1,12 +1,43 @@
 return {
-  "williamboman/mason-lspconfig.nvim",
-  opts = {
-    ensure_installed = {
-      "bashls",
-      "clangd",
-      "lua_ls",
-      "pyright",
-      "solargraph",
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+        "bashls",
+        "clangd",
+        "cssls",
+        "html",
+        "jsonls",
+        "lua_ls",
+        "neocmake",
+        "pyright",
+        "solargraph",
+        "tailwindcss",
+        "tsserver",
+      },
     },
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    opts = {
+      ensure_installed = {
+        "codelldb",
+        "prettierd",
+        "stylua",
+        "ruff-lsp",
+      },
+    },
+    config = function(_, opts)
+      local mason_null_ls = require "mason-null-ls"
+      local null_ls = require "null-ls"
+      mason_null_ls.setup(opts)
+      mason_null_ls.setup {
+        prettierd = function()
+          null_ls.register(null_ls.builtins.formatting.prettierd.with {
+            extra_filetypes = { "json", "markdown", "css" },
+          })
+        end,
+      }
+    end,
   },
 }
