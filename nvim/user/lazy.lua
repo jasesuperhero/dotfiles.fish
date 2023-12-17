57de069065e15925f0413ec7e247b79d4296737f
Daniel Kalintsev <d.kalintsev@gmail.com>
2023-12-17 12:31:41 +0100

    chore(nvim): some updates for the lazy configuration

--- nvim/user/lazy.lua
+++ nvim/user/lazy.lua
@@ -1,5 +1,9 @@
 return {
   defaults = { lazy = true },
+  change_detection = {
+    enabled = true,
+    notify = true,
+  },
   performance = {
     rtp = {
       -- customize default disabled vim plugins
