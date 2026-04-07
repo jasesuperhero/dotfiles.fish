88dfb4c224023aa8c87fdcc46c2a74ddffbe9737
Daniel Kalintsev <d.kalintsev@gmail.com>
2026-04-07 23:03:13 +0100

    feat(nvim): disable tabufline tab bar

--- nvim/config/lua/chadrc.lua
+++ nvim/config/lua/chadrc.lua
@@ -11,10 +11,11 @@ M.base46 = {
 }
 
 -- M.nvdash = { load_on_startup = true }
--- M.ui = {
---       tabufline = {
---          lazyload = false
---      }
--- }
+
+M.ui = {
+  tabufline = {
+    enabled = false,
+  },
+}
 
 return M
