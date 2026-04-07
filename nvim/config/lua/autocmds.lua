9d27f988a8565e946c9a2de3cfadd6e248497d8e
Daniel Kalintsev <d.kalintsev@gmail.com>
2026-04-07 22:37:40 +0100

    feat(nvim): port options and autocmds from nvim-v2
    Options: relativenumber, scrolloff=18, sidescrolloff=3, wrap=false,
    listchars, foldcolumn=1, shiftround, swapfile=false, cmdheight=0,
    timeoutlen=250, undolevels/undoreload
    
    Autocmds: yank highlight, auto-create dirs, no auto-comment leader,
    spell+wrap for prose, conceallevel=0 for JSON, colorcolumn for Python/C

--- nvim/config/lua/autocmds.lua
+++ nvim/config/lua/autocmds.lua
@@ -1,1 +1,43 @@
 require "nvchad.autocmds"
+
+local autocmd = vim.api.nvim_create_autocmd
+
+-- Highlight text on yank
+autocmd("TextYankPost", {
+  callback = function() vim.highlight.on_yank { higroup = "IncSearch", timeout = 300 } end,
+})
+
+-- Auto-create intermediate directories when saving a file
+autocmd("BufWritePre", {
+  callback = function(event)
+    if event.match:match "^%w%w+:[\\/][\\/]" then return end
+    local file = vim.uv.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+  end,
+})
+
+-- Don't auto-insert comment leader on newline
+autocmd("BufEnter", {
+  callback = function() vim.opt_local.formatoptions:remove { "c", "r", "o" } end,
+})
+
+-- Spell check and soft wrap for prose filetypes
+autocmd("FileType", {
+  pattern = { "gitcommit", "markdown", "text" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = true
+  end,
+})
+
+-- Fix conceallevel for JSON (prevent quotes being hidden)
+autocmd("FileType", {
+  pattern = { "json", "jsonc", "json5" },
+  callback = function() vim.opt_local.conceallevel = 0 end,
+})
+
+-- Color column guide for Python and C/C++
+autocmd("FileType", {
+  pattern = { "python", "rst", "c", "cpp" },
+  callback = function() vim.opt_local.colorcolumn = "80" end,
+})
--- nvim/config/lua/options.lua
+++ nvim/config/lua/options.lua
@@ -1,6 +1,42 @@
 require "nvchad.options"
 
+local opt = vim.opt
+local o = vim.o
+
 -- nvim-ufo requires foldlevel to be high so it can manage folds itself
-vim.o.foldlevel = 99
-vim.o.foldlevelstart = 99
-vim.o.foldenable = true
+o.foldlevel = 99
+o.foldlevelstart = 99
+o.foldenable = true
+o.foldcolumn = "1"
+
+-- Line numbers
+o.relativenumber = true
+
+-- Scrolling
+o.scrolloff = 18
+o.sidescrolloff = 3
+
+-- Indentation
+o.shiftround = true
+
+-- Display
+o.wrap = false
+o.cmdheight = 0
+opt.list = true
+opt.listchars = {
+  tab = "┊ ",
+  trail = "·",
+  extends = "»",
+  precedes = "«",
+  nbsp = "×",
+}
+
+-- Performance
+o.timeoutlen = 250
+
+-- Files
+o.swapfile = false
+
+-- Persistent undo
+o.undolevels = 1000
+o.undoreload = 10000
