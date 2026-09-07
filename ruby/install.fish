20162edb0df2588a59a85ccbe7b3950cabe36266
Daniel Kalintsev <d.kalintsev@gmail.com>
2026-09-07 12:44:40 +0100

    chore(ruby): install precompiled ruby binaries
    Set mise ruby.compile=false to use precompiled binaries (faster, and the
    default from mise 2026.8.0 onwards).

--- ruby/install.fish
+++ ruby/install.fish
@@ -1,5 +1,9 @@
 #!/usr/bin/env fish
 
+# Use precompiled Ruby binaries instead of compiling from source
+# (faster, and the default from mise 2026.8.0 onwards).
+mise settings ruby.compile=false
+
 mise install --yes ruby@latest
 mise use --global ruby@latest
 
