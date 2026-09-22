ed2c004415aaf64baf8c062268ded6b09f8603fc
Daniel Kalintsev <d.kalintsev@gmail.com>
2026-09-22 10:36:33 +0100

    chore(eza): drop --icons from eza abbreviations

--- eza/conf.d/eza_abbr.fish
+++ eza/conf.d/eza_abbr.fish
@@ -1,12 +1,12 @@
 #!/usr/bin/env fish
 
 if command -qa eza
-    abbr -a ls 'eza --icons'
-    abbr -a lg 'eza --icons --git'
-    abbr -a l 'eza -lah --icons --git'
-    abbr -a la 'eza -la --icons --git'
-    abbr -a ll 'eza -lah --icons --git'
-    abbr -a lt 'eza -lT --icons --git'
+    abbr -a ls eza
+    abbr -a lg 'eza --git'
+    abbr -a l 'eza -lah --git'
+    abbr -a la 'eza -la --git'
+    abbr -a ll 'eza -lah --git'
+    abbr -a lt 'eza -lT --git'
 else
     abbr -a l 'ls -lAh'
     abbr -a la 'ls -A'
